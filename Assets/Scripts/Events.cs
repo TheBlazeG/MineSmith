6e2f691b7cd927f34152f31d6db818cc724f81c0
TheBlaze <santi.volcanos@gmail.com>
2025-01-22 09:54:25 -0600

    added event mineral and 2 events

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Events : MonoBehaviour
 {
+    public GameObject Megamineral;
    
     // Start is called before the first frame update
     void Start()
     {
-       
+        StartCoroutine(EventTimer());
 
     }
 
@@ -23,7 +26,9 @@ public class Events : MonoBehaviour
     {
         while (true)
         {
+            Vector3 spawn = new Vector3(Random.Range(-7,7), Random.Range(-4, 4), 0);
             Debug.Log("Evento");
+            Instantiate(Megamineral, spawn,Quaternion.identity);
             yield return new WaitForSeconds(10);
         }
     }
